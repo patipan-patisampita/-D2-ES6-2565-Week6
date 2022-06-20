207853b369199a0f6239b6e808c7be48cda708d9
patipan-patisampita <patipan.patisampita@gmail.com>
2022-06-20 17:32:57 +0700

    update

--- object3.js
+++ object3.js
@@ -1,15 +1,25 @@
 let obj1 = {
-    1:"Hello",
-    2:"World"
+    name1: "Hello",
+    name2: "World"
 }
 let obj2 = {
-    3:"Welcome",
-    4:"to"
+    name3: "Welcome",
+    name4: "to"
 }
 let obj3 = {
-    5:"Metaverse",
+    name5: "Metaverse",
 }
-console.log(obj1,obj2,obj3)
-//Object.assign()
-let a = Object.assign(obj1,obj2,obj3)
-console.log(a)
+//3.Object Destructurin
+let { name1, name2 } = obj1
+let { name3, name4 } = obj1
+let { name5 } = obj1
+console.log(name1, name2)
+
+//0.console.log(obj1,obj2,obj3)
+//1.Object.assign()
+let a = Object.assign(obj1, obj2, obj3)
+//console.log(a)
+
+//2.Object Spread
+let b = { ...obj1, ...obj2, ...obj3 }
+//console.log(b)
