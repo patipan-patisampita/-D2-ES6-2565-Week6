c5ab7b47e2fc018ea645d17edbb6a363b57fc92d
patipan-patisampita <patipan.patisampita@gmail.com>
2022-06-20 17:19:27 +0700

    update

---
+++ object3.js
@@ -0,0 +1,15 @@
+let obj1 = {
+    1:"Hello",
+    2:"World"
+}
+let obj2 = {
+    3:"Welcome",
+    4:"to"
+}
+let obj3 = {
+    5:"Metaverse",
+}
+console.log(obj1,obj2,obj3)
+//Object.assign()
+let a = Object.assign(obj1,obj2,obj3)
+console.log(a)
